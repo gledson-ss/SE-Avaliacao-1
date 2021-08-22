f10aeaccd1b847feb7304c2b85341d10d7b42148
gledson-ss <43425195+gledson-ss@users.noreply.github.com>
2021-08-21 21:34:47 -0300

    add question

--- respostas/questao3/questao3.c
+++ respostas/questao3/questao3.c
@@ -2,11 +2,11 @@
 #include <util/delay.h>
 int main(void) {	
   // pino led agora esta como output
-	DDRB |= (1 << DDB5);
+  DDRB |= (1 << DDB5);
   int True = 1;
-	while (True) {
-		PORTB |= (1 << PINB5);
-	}
+  while (True) {
+    PORTB |= (1 << PINB5);
+  }
 }
 
 //Escreva um programa em linguagem C que acenda o LED, ilustrado na figura abaixo.
