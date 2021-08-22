906d4ef0745216af10893c9ea5b0ee620b833976
gledson-ss <43425195+gledson-ss@users.noreply.github.com>
2021-08-21 21:32:55 -0300

    add question

--- respostas/questao1/questao1.c
+++ respostas/questao1/questao1.c
@@ -3,19 +3,19 @@
 #include <util/delay.h>
 
 ISR(INT0_vect) {
-	PORTC ^= 0b00100000; 
+  PORTC ^= 0b00100000; 
 }
 
 int main(void)
 {
   PORTD = 0xFF;
-	DDRD = 0x00;
-	DDRC = 0b00100000; 
-	EICRA = 0b00000010;
-	EIMSK = 0b00000001;
-	sei();
+  DDRD = 0x00;
+  DDRC = 0b00100000; 
+  EICRA = 0b00000010;
+  EIMSK = 0b00000001;
+  sei();
   int True = 1;
-	while(True){}
+  while(True){}
 }
 
 //Escreva um código para configurar a interrupção externa apenas para o pino 28 do ATmega328P ilustrado na figura abaixo
