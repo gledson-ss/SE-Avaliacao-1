79c043977fa0e274aeb1f6f9dbd8a14b84951791
gledson-ss <43425195+gledson-ss@users.noreply.github.com>
2021-08-21 21:34:17 -0300

    add question

--- respostas/questao2/questao2.c
+++ respostas/questao2/questao2.c
@@ -2,15 +2,15 @@
 #include <util/delay.h>
 
 int main(void) {	
-	// setando botão entrada
-	DDRD &= ~(1 << DDD3);
+  // setando botão entrada
+  DDRD &= ~(1 << DDD3);
 
-	// Fica atualizando valor da variavel x eternamente para sempre termos o valor atualizado
+  // Fica atualizando valor da variavel x eternamente para sempre termos o valor atualizado
   // laço que sempre tem o valor mudado
   int True = 1;
-	while (True) {
-		bool estado = (PIND & (1 << DDD3));
-	}
+  while (True) {
+    bool estado = (PIND & (1 << DDD3));
+  }
 }
 
 //Escreva um código em C para ler o estado do chave ‘S’, ilustrado na figura abaixo, e guarde na variável ‘estado’ do tipo ‘bool’.
